c558feed056dba7f30a25d8c82b8129b4b3cbb03
richb999git <richb999@tiscali.co.uk>
2019-01-18 08:01:57 +0000

    added db connection debugging

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-
+echo "in dbh require";
 //Get Heroku ClearDB connection information
 if (!empty(getenv("CLEARDB_DATABASE_URL"))) { // or "" or NULL
     $cleardb_url  = parse_url(getenv("CLEARDB_DATABASE_URL"));
@@ -8,16 +8,24 @@ if (!empty(getenv("CLEARDB_DATABASE_URL"))) { // or "" or NULL
     $dBUsername   = $cleardb_url["user"];
     $dBPassword   = $cleardb_url["pass"];
     $dBName       = substr($cleardb_url["path"],1);
+    echo "in if. in heroku db settings";
 }
 else {  
     $servername = "localhost";
     $dBUsername = "root";
     $dBPassword = "";
     $dBName = "loginsystem";
+    echo "in if. in localhost db settings";
 }
 
 $conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
-
+echo "after db connection";
 if (!$conn) {
+    echo "CONNECTION FAILED";
     die("Connection failed: ".mysqli_connect_error());
+    echo "CONNECTION FAILED - AFTER DIE";
 }
+
+echo "CONNECTION FAILED - AFTER DIE";
+
+?>
--- seeLog.php
+++ seeLog.php
@@ -9,7 +9,44 @@
     if (isset($_SESSION["userId"])) {
     
     echo "before dbh require";
-    require "/includes/dbh.inc.php";
+
+    //require "/includes/dbh.inc.php";
+
+//////////////////////////////////////////////// test ////////////////////////////////////////
+
+
+    echo "in dbh require";
+    //Get Heroku ClearDB connection information
+    if (!empty(getenv("CLEARDB_DATABASE_URL"))) { // or "" or NULL
+        $cleardb_url  = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        $servername   = $cleardb_url["host"];
+        $dBUsername   = $cleardb_url["user"];
+        $dBPassword   = $cleardb_url["pass"];
+        $dBName       = substr($cleardb_url["path"],1);
+        echo "in if. in heroku db settings";
+    }
+    else {  
+        $servername = "localhost";
+        $dBUsername = "root";
+        $dBPassword = "";
+        $dBName = "loginsystem";
+        echo "in if. in localhost db settings";
+    }
+    
+    $conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
+    echo "after db connection";
+    if (!$conn) {
+        echo "CONNECTION FAILED";
+        die("Connection failed: ".mysqli_connect_error());
+        echo "CONNECTION FAILED - AFTER DIE";
+    }
+    
+    echo "END of require section - connection must be good";
+
+
+//////////////////////////////////////////////// test ////////////////////////////////////////
+
+
     echo "after dbh require";
 
     if(isset($_POST["whichErgs"])) {        // from report choice page
