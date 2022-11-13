a3e60c6721845be4b9bb7a376cb81ec9cee88ffc
Richard Braunton <richard@computech.software>
2022-11-13 16:24:53 +0000

    Set port when connecting to mysql (local port changed to 3310 - default is 3306)

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -7,17 +7,18 @@ if (!empty(getenv("CLEARDB_DATABASE_URL"))) { // or "" or NULL
     $dBUsername   = $cleardb_url["user"];
     $dBPassword   = $cleardb_url["pass"];
     $dBName       = substr($cleardb_url["path"],1);
+    $port         = 3306; // might not need this
 }
-else {  
+else {
     $servername = "localhost";
     $dBUsername = "root";
     $dBPassword = "";
     $dBName = "loginsystem";
+    $port = 3310; // for my local mysql because mariadb is on 3306
 }
 
-$conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
+$conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName, $port);
 
 if (!$conn) {
     die("Connection failed: ".mysqli_connect_error());
 }
-
