dd0b6f7a2b26edb6ad1391fa78bb2e55f0a22f2a
Richard Braunton <richard@computech.software>
2023-04-07 18:27:30 +0100

    Change database environment variables

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,13 +1,13 @@
 <?php
 
-//Get Heroku ClearDB connection information
-if (!empty(getenv("CLEARDB_DATABASE_URL"))) { // or "" or NULL
-    $cleardb_url  = parse_url(getenv("CLEARDB_DATABASE_URL"));
-    $servername   = $cleardb_url["host"];
-    $dBUsername   = $cleardb_url["user"];
-    $dBPassword   = $cleardb_url["pass"];
-    $dBName       = substr($cleardb_url["path"],1);
-    $port         = 3306; // might not need this
+//Get Railway.app connection information // Heroku ClearDB connection information
+if (!empty(getenv("MYSQL_URL"))) { //CLEARDB_DATABASE_URL"))) { // or "" or NULL
+    //$cleardb_url  = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $servername   = getenv("MYSQL_URL"); // MYSQLHOST // $cleardb_url["host"];
+    $dBUsername   = getenv("MYSQLUSER"); // $cleardb_url["user"];
+    $dBPassword   = getenv("MYSQLPASSWORD"); // $cleardb_url["pass"];
+    $dBName       = getenv("MYSQLDATABASE"); // substr($cleardb_url["path"],1);
+    $port         = getenv("MYSQLPORT"); // 3306; // might not need this
 }
 else {
     $servername = "localhost";
