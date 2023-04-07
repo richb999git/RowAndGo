fae8ed7af7b63ed3b875acde4d0b0c18e0c6e057
Richard Braunton <richard@computech.software>
2023-04-07 20:06:24 +0100

    Tidy code

--- index.php
+++ index.php
@@ -1,80 +1,68 @@
 <?php
     require "header.php";
 ?>
+<main>
+    <?php
+        if (isset($_GET["error"])) {
 
-    <main>
-        <?php
-
-            if (isset($_GET["error"])) {                
-
-                if ($_GET["error"] == "notloggedin") {
-                    $errorMessage = "You need to be logged in!";
-                } else if ($_GET["error"] == "DELETE_OK") {
-                    $errorMessage = "Score deleted successfully";
-                } else if ($_GET["error"] == "DELETE_ERROR") {
-                    $errorMessage = "Error deleting score";
-                } else if ($_GET["error"] == "nodirectaccess") {
-                    $errorMessage = "Access denied";
-                } else if ($_GET["error"] == "sqlerror") {
-                    $errorMessage = "Database error when updating. Please try again.";
-                } else if ($_GET["error"] == "REPORT_ERROR") {
-                    $errorMessage = "Database error when reporting. Please try again.";
-                } else if ($_GET["error"] == "invalidScoreID") {
-                    $errorMessage = "Invalid score ID";
-                } else { $errorMessage = "Error";} ?>
-                    
-                    <div class="row">
-                        <div class="col s12 m4 offset-m4">
-                            <div class="card-panel teal center-align z-depth-3">
-                                <span class="white-text"><?= $errorMessage ?></span>
-
-                     
-                    <?php if ($errorMessage == "Score deleted successfully") { ?>
-                        
-                        <p><a class="btn white-text" href="javascript:history.go(-2)">Back to scores</a></p>
-
-                    <?php } ?>
-                    
-                            </div>
+            if ($_GET["error"] == "notloggedin") {
+                $errorMessage = "You need to be logged in!";
+            } else if ($_GET["error"] == "DELETE_OK") {
+                $errorMessage = "Score deleted successfully";
+            } else if ($_GET["error"] == "DELETE_ERROR") {
+                $errorMessage = "Error deleting score";
+            } else if ($_GET["error"] == "nodirectaccess") {
+                $errorMessage = "Access denied";
+            } else if ($_GET["error"] == "sqlerror") {
+                $errorMessage = "Database error when updating. Please try again.";
+            } else if ($_GET["error"] == "REPORT_ERROR") {
+                $errorMessage = "Database error when reporting. Please try again.";
+            } else if ($_GET["error"] == "invalidScoreID") {
+                $errorMessage = "Invalid score ID";
+            } else { $errorMessage = "Error";} ?>
+                <div class="row">
+                    <div class="col s12 m4 offset-m4">
+                        <div class="card-panel teal center-align z-depth-3">
+                            <span class="white-text"><?= $errorMessage ?></span>
+                <?php if ($errorMessage == "Score deleted successfully") { ?>
+                    <p><a class="btn white-text" href="javascript:history.go(-2)">Back to scores</a></p>
+                <?php } ?>
                         </div>
                     </div>
-
-                    <?php
-                    require "seeLog/sqlSpinner.php";
-
-            } else if (isset($_GET["update_success"])) { ?>
-                    
-                    <div class="row">
-                        <div class="col s12 m4 offset-m4">
-                            <div class="card-panel teal center-align z-depth-3">
-                                <span class="white-text">Score update successful</span> 
-                            </div>
-                        </div>
-                    </div>
-                    
-            <?php
-            } else if (isset($_GET["edit_success"])) { ?>
-                    
-                    <div class="row">
-                        <div class="col s12 m6 l4 offset-m3 offset-l4" >
-                            <div class="card-panel teal center-align z-depth-3">
-                                <p class="white-text">Score update successful</p> 
-                                <p><a class="btn white-text" href="javascript:history.go(-2)">Back to scores</a></p>
-                            </div>
-                        </div>
-                    </div>
-            <?php
-                    require "seeLog/sqlSpinner.php";
-            }
-            ?>
-                <div class="center-align">
-                    <img width=100% src="pics/row&go_front.jpg" alt="Row&Go! Rower going through his paces on the erg!">
-                    <h2>Row&Go! Log your erg scores and compare with other rowers!</h2>
                 </div>
-        <br/>
-        <div class="center-align"><img width=95% id="screenshot" src="pics/row&go_example.png" alt="example screen shot of Row&Go!"></div>
-        <br/>
-    </main>
-    
+
+                <?php
+                require "seeLog/sqlSpinner.php";
+
+        } else if (isset($_GET["update_success"])) { ?>
+                <div class="row">
+                    <div class="col s12 m4 offset-m4">
+                        <div class="card-panel teal center-align z-depth-3">
+                            <span class="white-text">Score update successful</span> 
+                        </div>
+                    </div>
+                </div>
+        <?php
+        } else if (isset($_GET["edit_success"])) { ?>
+                <div class="row">
+                    <div class="col s12 m6 l4 offset-m3 offset-l4" >
+                        <div class="card-panel teal center-align z-depth-3">
+                            <p class="white-text">Score update successful</p> 
+                            <p><a class="btn white-text" href="javascript:history.go(-2)">Back to scores</a></p>
+                        </div>
+                    </div>
+                </div>
+        <?php
+                require "seeLog/sqlSpinner.php";
+        }
+        ?>
+            <div class="center-align">
+                <img width=100% src="pics/row&go_front.jpg" alt="Row&Go! Rower going through his paces on the erg!">
+                <h2>Row&Go! Log your erg scores and compare with other rowers!</h2>
+            </div>
+    <br/>
+    <div class="center-align"><img width=95% id="screenshot" src="pics/row&go_example.png" alt="example screen shot of Row&Go!"></div>
+    <br/>
+</main>
 <?php
     require "footer.php";
