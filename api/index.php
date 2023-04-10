c1899cc1289ac337eb188b3d4ecbabaed0c805f9
Richard Braunton <richard@computech.software>
2023-04-10 18:30:27 +0100

    Update routes

--- api/index.php
+++ api/index.php
@@ -6,6 +6,16 @@
 
 if (substr($_SERVER['REQUEST_URI'], 0, 8) === '/contact') {
     require __DIR__ . '/../contact.php';
+} else  if (substr($_SERVER['REQUEST_URI'], 0, 6) === '/login') {
+    require __DIR__ . '/../login.php';
+} else  if (substr($_SERVER['REQUEST_URI'], 0, 7) === '/signup') {
+    require __DIR__ . '/../signup.php';
+} else  if (substr($_SERVER['REQUEST_URI'], 0, 9) === '/addscore') {
+    require __DIR__ . '/../addscore.php';
+} else  if (substr($_SERVER['REQUEST_URI'], 0, 11) === '/viewScores') {
+    require __DIR__ . '/../viewScores.php';
+} else  if (substr($_SERVER['REQUEST_URI'], 0, 16) === '/downloadResults') {
+    require __DIR__ . '/../downloadResults.php';
 } else {
     require __DIR__ . '/../index.php';
 }
