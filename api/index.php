be11754360b2c3c7d53e98a5c0b7f61b76a62d60
Richard Braunton <richard@computech.software>
2023-04-10 18:23:57 +0100

    Updae routes

--- api/index.php
+++ api/index.php
@@ -4,7 +4,7 @@
 //require __DIR__ . '/../index.php';
 
 
-if (str_starts_with($_SERVER['REQUEST_URI'], '/contact')) {
+if (substr($_SERVER['REQUEST_URI'], 0, 8) === '/contact') {
     require __DIR__ . '/../contact.php';
 } else {
     require __DIR__ . '/../index.php';
