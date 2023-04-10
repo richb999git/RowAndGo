c179c73f395d66015654309f72f6e0c6e998fdeb
Richard Braunton <richard@computech.software>
2023-04-10 18:31:08 +0100

    Remove commented out code

--- api/index.php
+++ api/index.php
@@ -1,8 +1,6 @@
 <?php
 
 // Forward Vercel requests to normal index.php
-//require __DIR__ . '/../index.php';
-
 
 if (substr($_SERVER['REQUEST_URI'], 0, 8) === '/contact') {
     require __DIR__ . '/../contact.php';
