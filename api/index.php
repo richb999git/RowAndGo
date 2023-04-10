1aaaec829c5e7d42b6a5b7933ed7e74b16faed00
Richard Braunton <richard@computech.software>
2023-04-10 19:28:37 +0100

    Revert routes

--- api/index.php
+++ api/index.php
@@ -2,24 +2,18 @@
 
 // Forward Vercel requests to normal index.php
 
-// if (substr($_SERVER['REQUEST_URI'], 0, 8) === '/contact') {
-//     require __DIR__ . '/../contact.php';
-// } else  if (substr($_SERVER['REQUEST_URI'], 0, 6) === '/login') {
-//     require __DIR__ . '/../login.php';
-// } else  if (substr($_SERVER['REQUEST_URI'], 0, 7) === '/signup') {
-//     require __DIR__ . '/../signup.php';
-// } else  if (substr($_SERVER['REQUEST_URI'], 0, 9) === '/addScore') {
-//     require __DIR__ . '/../addScore.php';
-// } else  if (substr($_SERVER['REQUEST_URI'], 0, 11) === '/viewScores') {
-//     require __DIR__ . '/../viewScores.php';
-// } else  if (substr($_SERVER['REQUEST_URI'], 0, 16) === '/downloadResults') {
-//     require __DIR__ . '/../downloadResults.php';
-// } else {
-//     require __DIR__ . '/../index.php';
-// }
-
-if ($_SERVER['REQUEST_URI'] === '/') {
-    require __DIR__ . '/../index.php';
+if (substr($_SERVER['REQUEST_URI'], 0, 8) === '/contact') {
+    require __DIR__ . '/../contact.php';
+} else  if (substr($_SERVER['REQUEST_URI'], 0, 6) === '/login') {
+    require __DIR__ . '/../login.php';
+} else  if (substr($_SERVER['REQUEST_URI'], 0, 7) === '/signup') {
+    require __DIR__ . '/../signup.php';
+} else  if (substr($_SERVER['REQUEST_URI'], 0, 9) === '/addScore') {
+    require __DIR__ . '/../addScore.php';
+} else  if (substr($_SERVER['REQUEST_URI'], 0, 11) === '/viewScores') {
+    require __DIR__ . '/../viewScores.php';
+} else  if (substr($_SERVER['REQUEST_URI'], 0, 16) === '/downloadResults') {
+    require __DIR__ . '/../downloadResults.php';
 } else {
-    require __DIR__ . '/..' . $_SERVER['REQUEST_URI'];
+    require __DIR__ . '/../index.php';
 }
