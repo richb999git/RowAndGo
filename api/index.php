a55e36766e4fcda3001b212fdaf807b19554c025
Richard Braunton <richard@computech.software>
2023-04-10 18:20:11 +0100

    update routes

--- api/index.php
+++ api/index.php
@@ -1,4 +1,11 @@
 <?php
 
 // Forward Vercel requests to normal index.php
-require __DIR__ . '/../index.php';
+//require __DIR__ . '/../index.php';
+
+
+if (str_starts_with($_SERVER['REQUEST_URI'], '/contact')) {
+    require __DIR__ . '/../contact.php';
+} else {
+    require __DIR__ . '/../index.php';
+}
